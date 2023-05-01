a5e466a260bb14f72e21b51bb197a243cc950c63
Joshua George <horheyinc8@gmail.com>
2023-05-01 23:18:37 +0200

    testing again for mongodb

--- config/session.config.js
+++ config/session.config.js
@@ -5,24 +5,24 @@ const MONGO_URI =
 
 module.exports = (app) => {
   app.set("trust proxy", 1);
-  // app.use(
-  //   session({
-  //     secret: "mySessSecret",
-  //     resave: false,
-  //     saveUninitialized: false,
-  //     cookie: {
-  //       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  //       secure: process.env.NODE_ENV === "production",
-  //       httpOnly: true,
-  //       maxAge: 60000 * 60 * 24, // 60 * 1000 ms === 1 day
-  //     },
-  //     //<===========this is where we save the session into the DB!!!! ===============>
-  //     store: MongoStore.create({
-  //       mongoUrl: MONGO_URI,
-  //       useNewUrlParser: true,
-  //       //ttl => time to live
-  //       ttl: 60 * 60 * 24, // 60sec * 60min * 24h => 1 day
-  //     }),
-  //   })
-  // );
+  app.use(
+    session({
+      secret: process.env.SESS_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        secure: process.env.NODE_ENV === "production",
+        httpOnly: true,
+        maxAge: 60000 * 60 * 24, // 60 * 1000 ms === 1 day
+      },
+      //<===========this is where we save the session into the DB!!!! ===============>
+      store: MongoStore.create({
+        mongoUrl: MONGO_URI,
+        useNewUrlParser: true,
+        //ttl => time to live
+        ttl: 60 * 60 * 24, // 60sec * 60min * 24h => 1 day
+      }),
+    })
+  );
 };
