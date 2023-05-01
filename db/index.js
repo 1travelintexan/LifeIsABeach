745655be7ade4da5807628d15b6d4e8e91c689e3
Joshua George <horheyinc8@gmail.com>
2023-05-01 21:45:44 +0200

    testing the .env

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/Kook-Club";
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((x) => {
     const databaseName = x.connections[0].name;
     console.log(`Connected to Mongo! Database name: "${databaseName}"`);
